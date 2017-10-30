4dbd7c4112a40691f1ce223056fe6a1cc5ec09d8
Bryan St. Amour <bryan.stamour@gmail.com>
2017-10-30 11:09:03 -0400

    moved algorithms to separate header

--- soft/genetic_algorithm.hxx
+++ soft/genetic_algorithm.hxx
@@ -28,6 +28,8 @@
 #ifndef BST_GA_HXX_
 #define BST_GA_HXX_
 
+#include "algorithm.hxx"
+
 #include <algorithm>
 #include <functional>
 #include <iterator>
@@ -37,40 +39,9 @@
 #include <utility>
 #include <vector>
 
+//------------------------------------------------------------------------------
+
 namespace soft::ga {
-
-//------------------------------------------------------------------------------
-
-template <typename Iter, typename Distance, typename Generator, typename Func>
-void sample_and_apply(Iter first, Iter last, Distance n, Generator g, Func f)
-{
-  using value_type = typename std::remove_reference_t<Iter>::value_type;
-  auto refs        = std::vector<value_type*>{};
-  refs.reserve(n);
-  std::sample(first, last, std::back_inserter(refs), n, g);
-  for (auto& x : refs) *x = f(*x);
-}
-
-template <typename Iter, typename Gen, typename ScoreFunc>
-auto rejection_sample(Iter first, Iter last, Gen gen, ScoreFunc f) -> Iter
-{
-  auto const num_elements  = std::distance(first, last);
-  auto const highest_value = *std::max_element(
-      first, last, [&](auto const& x, auto const& y) { return f(x) < f(y); });
-  auto dist        = std::uniform_int_distribution<>{0, num_elements - 1};
-  using value_type = decltype(highest_value);
-
-  while (true) {
-    auto pos = std::next(first, dist(gen)); // Move to a random element.
-    auto val_dist =
-        std::uniform_real_distribution<value_type>{0, highest_value};
-    auto y = val_dist(gen); // sample.
-    if (y < f(*pos))        // accept or reject.
-      return *pos;
-  }
-}
-
-//------------------------------------------------------------------------------
 
 struct algorithm_traits {
   int iterations;
@@ -97,20 +68,21 @@ auto run_genetic_algorithm(Pool const& pool, ScoreFunc f, MutateFunc m,
 
     // 1. Mutate.
     auto mutate_dist = std::uniform_int_distribution<>{0, traits.max_mutations};
-    sample_and_apply(begin(scored_items), end(scored_items), mutate_dist(gen),
-                     gen, [&](auto p) {
-                       *p.first = m(*p.first); // mutate...
-                       p.second = f(*p.first); // then re-score
-                       return p;
-                     });
+    algorithm::sample_and_apply(begin(scored_items), end(scored_items),
+                                mutate_dist(gen), gen, [&](auto p) {
+                                  *p.first = m(*p.first); // mutate...
+                                  p.second = f(*p.first); // then re-score
+                                  return p;
+                                });
 
     // 2. Crossover.
     auto cross_dist = std::uniform_int_distribution<>{0, traits.max_crossovers};
     auto old_end    = end(scored_items);
     std::generate_n(std::back_inserter(scored_items), cross_dist(gen), [&] {
       auto picker = [&] {
-        return rejection_sample(begin(scored_items), old_end, gen,
-                                [](auto const& x) { return x.second; });
+        return algorithm::rejection_sample(
+            begin(scored_items), old_end, gen,
+            [](auto const& x) { return x.second; });
       };
 
       auto parent_1 = picker();
